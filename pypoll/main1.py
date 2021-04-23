b95ad88c3e8e15df5ff909840c316302c8b5bb15
Saumya Pandey <saumyashukla20@gmail.com>
2021-04-22 20:35:00 -0400

    code is completely working and printing on screen . figuring out how to print on a txt file

--- pypoll/main1.py
+++ pypoll/main1.py
@@ -2,33 +2,62 @@ import os
 import csv
 #defining variables
 total_votes=0
-poll_result={}
 cand_name=[]
+cand_vote=[]
 unique_cand=[]
+# Defining path to read CSV file.
 csv_path=os.path.join("Resources", "election_data.csv")
-#reading the csv file 
+
+# Reading the csv file 
 with open(csv_path) as csvfile:
     csvreader= csv.reader(csvfile,delimiter=",")
-    print(csvreader)
     csv_header=next(csvreader)
-    print(csv_header)
-    
+
+
+# Loop to get Total Votes and Create Two list 
+# 1)Cand_name(has the list of all occurances of candidate)
+# 2)cand_vote(total voter ID lis)    
     for row in csvreader:
         total_votes+=1
         cand_name.append(row[2])
-        
-#print (cand_name)
+        cand_vote.append(row[0])
 
-list_1 = []
+
+#getting Unique candidate
+candidate_list = []
 for candidate in set(cand_name):
-    list_1.append(candidate)
-print(list_1)
-#for i in cand_name:
- #   if i not in cand_name:
-  #   unique_cand.append(i)
-#print(unique_cand) 
-
-#print(total_votes)
+    candidate_list.append(candidate)
 
 
+#printing Total votes
+print(f'     The Elections Results          ')
+print(f'-----------------------------------------------------------')
+print(f'Total Votes are  {total_votes}')
+print(f'-----------------------------------------------------------')
+
+
+# loop to get the total number of votes, percentage for each candidate
+l=len(candidate_list)
+percent_value=[]
+count=0
+count_tot_vote=[]
+tl=len(cand_name)
+for y in range(l):
+    count=0
+    for x in range(tl):
+        if cand_name[x]==candidate_list[y]:
+                count+=1
+    count_tot_vote.append(count)
+    percent_value.append((count/total_votes)*100)
+    print(f'Mr. {candidate_list[y]} got vote percentage: {percent_value[y]} with a total vote_count: {count_tot_vote[y]} ')
+
+# finally, finding the winner and printing it
+Max_vote= max(count_tot_vote)
+winner=candidate_list[(count_tot_vote.index(Max_vote))]
+print(f'-----------------------------------------------------------')
+print(f'And the Winner is....DrumRoll.......... Mr.{winner} !!!!!!!!')
+print(f'-----------------------------------------------------------')
+
+csv_outpath=os.path.join("analysis",output.txt)
+with open(csv_output,)
 
